c737c63cb760a2a6f64fcb2d928582b3c23ed640
Charlie Roche <charlie.roche@gmail.com>
2021-02-01 14:15:31 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_module_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[" + DateTime.Now + "] Hello from app-module-01!");
+        }
+    }
+}
